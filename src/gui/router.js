6a839e4f647c11bcc63420dce3f1d8aec4a4f5b2
Iacovlev Alexander <jacoby3210@gmail.com>
2024-10-10 00:58:07 +0300

    error page

--- src/gui/router.js
+++ src/gui/router.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Blog from './pages/blog/Blog.vue';
 import Start from './pages/start/Start.vue';
+import Error from './pages/error/Error.vue';
 
 const routes = [
   {component: Start, name: 'Home', path: '/',},
   {component: Blog, name: 'Blog', path: '/blog',},
+  {component: Error, name: 'Error', path: '/:pathMatch(.*)*',},
 ];
 const router = createRouter({history: createWebHistory(), routes});
 
